0ec2ae66c2b6694b3c5788c1e3bb5740832a7a8e
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-12-12 04:42:08 +0100

    first deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>Welcome to Freecodecamp Ibadan Meetup</h1>
+    <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
+  </div>
+)
