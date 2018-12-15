cb5d51bbf686999947dbd9be97dd338dc2693b02
stevoo24 <yemifabusuyi@gmail.com>
2018-12-15 15:23:18 +0100

    created the about.js page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,7 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1>Welcome to FreeCodeCamp Ibadan Meetup</h1>
-    <p>The FreeCodeCamp Ibadan Meetup</p>
+    <h1>Welcome to freeCodeCamp Ibadan Meetup</h1>
     <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
   </div>
 )
