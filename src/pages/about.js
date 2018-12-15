78e76d86076ff4f7f2139514bcb2ac6f80f0afec
Yemi Fabusuyi <43810346+stevoo24@users.noreply.github.com>
2018-12-15 15:22:01 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from "react"
 export default () => (
   <div>
     <h1>Welcome to FreeCodeCamp Ibadan Meetup</h1>
-    <p>The FreeCodeCamp Ibadan Meetup</p>
+    <p>The FreeCodeCamp Ibadan Meetup !</p>
     <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
   </div>
 )
