2728ba33f717660bd37b7df408f2491bc44c169f
stevoo24 <yemifabusuyi@gmail.com>
2018-12-15 15:38:51 +0100

    Wrote a little about the purpose of the meetup.

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import React from "react"
 export default () => (
   <div>
     <h1>Welcome to freeCodeCamp Ibadan Meetup</h1>
+    <p>The freeCodeCamp Ibadan Meetup is a gathering where beginners, intermediate, and advanced JavaScript developers meet to know one another, discuss the challenges faced and get information on the latest news and changes about JavaScript and JavaScript frameworks. <br /> It comes up every month on a Saturday at designated location but currently holds at the Edulight Consulting office, 1st floor, Leventis Building, Lebanon Street Dugbe, Ibadan, Nigeria.</p>
     <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
   </div>
 )
