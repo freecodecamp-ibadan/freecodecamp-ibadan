fd4d94cb8f51ebfce47a39f7f53ed6bd2aa78cbc
stevoo24 <yemifabusuyi@gmail.com>
2018-12-15 15:16:40 +0100

    changed Freecodecamp to camelCasing text style in h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1>Welcome to Freecodecamp Ibadan Meetup</h1>
+    <h1>Welcome to freeCodeCamp Ibadan Meetup</h1>
     <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
   </div>
 )
