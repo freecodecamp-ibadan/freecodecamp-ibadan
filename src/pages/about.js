eb121bde4acf16f0be52df9d1a228b6223982179
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-02-10 20:35:52 +0100

    👌 IMPROVE:  update the index and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,13 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default () => (
   <div>
+    <Link to="/" style={{
+      textDecoration: 'none'
+    }}>
+      <h1>Welcome to Freecodecamp Ibadan Meetup</h1>
+    </Link>
     <h1>About</h1>
     <p>The freeCodeCamp Ibadan Meetup is a gathering where beginners, intermediate, and advanced JavaScript developers meet to know one another, discuss the challenges faced and get information on the latest news and changes about JavaScript and JavaScript frameworks. <br /> It comes up every month on a Saturday at designated location but currently holds at the Edulight Consulting office, 1st floor, Leventis Building, Lebanon Street Dugbe, Ibadan, Nigeria.</p>
     <p> &copy; All right Reserved. Made with Love by the Ibadan Campers</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,51 @@
 import React from "react"
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Center = styled.div`
+  height: 70vh;
+  margin: 0 300px;
+  padding: 20px;
+  text-align: center;
+  background-color: #ffffff;
+  color: #222222;
+  &:before {
+    content: '';
+    background-color: #006400;
+    width: 100px;
+    position: absolute;
+    height: 70vh;
+    right: 210px;
+    top: 80px;
+  }
+  &:after {
+    background-color: #006400;
+    content: '';
+    width: 100px;
+    position: absolute;
+    height: 70vh;
+    left: 210px;
+    top: 80px;
+  }
+  p {
+    font-size: 4em;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 export default () => (
   <div>
+  <Link to="/">
     <h1>Welcome to Freecodecamp Ibadan Meetup</h1>
-    <p>Site under construction</p>
-    <Link to="/about">
-      Go to the About Page For More Details
-    </Link>
+  </Link>
+    <Center>
+      <p>Site under construction</p><br />
+      <Link to="/about">
+        Go to the About Page For More Details
+      </Link>
+    </Center>
     <p> &copy; All right Reserved. Made with <span role="img" aria-label="heart emoji">💖</span> by the Ibadan Campers</p>
   </div>
 )
