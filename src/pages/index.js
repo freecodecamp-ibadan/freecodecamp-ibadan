8f0b494d3d06cdcebc071f0c98366cf3bf129b64
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-02-10 16:57:39 +0100

    📦 NEW: Add  new link to the About Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default () => (
   <div>
     <h1>Welcome to Freecodecamp Ibadan Meetup</h1>
-    <p> &copy; All right Reserved. Made with 💖 by the Ibadan Campers</p>
+    <p>Site under construction</p>
+    <Link to="/about">
+      Go to the About Page For More Details
+    </Link>
+    <p> &copy; All right Reserved. Made with <span role="img" aria-label="heart emoji">💖</span> by the Ibadan Campers</p>
   </div>
 )
